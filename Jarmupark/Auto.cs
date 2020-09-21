913aff63dea91418a1d199c0fbd0d6117521fa24
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-21 13:04:18 +0200

    Működő Auto osztály

---
+++ Jarmupark/Auto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarmupark
+{
+    class Auto
+    {
+        private string szin, tipus;
+        private double uzemAnyag, fogyaszt, km, megthetetoKm;
+        public Auto(string szin, string tipus, double fogyaszt)
+        {
+            this.szin = szin;
+            this.tipus = tipus;
+            this.fogyaszt = fogyaszt;
+            this.uzemAnyag = 0;
+            this.km = 0;
+            this.megthetetoKm = 0;
+        }
+        private void Megteheto()
+        {
+            megthetetoKm = (uzemAnyag / fogyaszt) * 100;
+        }
+        public void Tankol(double mennyit)
+        {
+            uzemAnyag += mennyit;
+            Megteheto();
+        }
+        public void Megy(double km)
+        {
+            this.km += km;
+            uzemAnyag -= (km * fogyaszt / 100);
+            Megteheto();
+        }
+        public void Allapot()
+        {
+            Console.WriteLine($"Típus: {tipus} \nSzín: {szin} \nKilométeróra: {km} \nÜzemanyag: {uzemAnyag} l \nMegtehető km: {megthetetoKm}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine();
+        }
+
+    }
+}
--- Jarmupark/Program.cs
+++ Jarmupark/Program.cs
@@ -10,6 +10,36 @@ namespace Jarmupark
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------Lada----------------");
+            Auto lada = new Auto("piros", "Lada2112", 11);
+
+            lada.Allapot();
+
+            lada.Tankol(20);
+
+            Console.WriteLine("Mennyit megy a Lada: ");
+            int km = int.Parse(Console.ReadLine());
+
+            lada.Megy(km);
+
+            lada.Allapot();
+
+            Console.WriteLine("-----------------Trabant----------------");
+
+            Auto trabant = new Auto("zöld", "Trabant 1.1", 8);
+
+            trabant.Allapot();
+
+            trabant.Tankol(20);
+
+            Console.WriteLine("Mennyit megy a Trabant: ");
+            int km1 = int.Parse(Console.ReadLine());
+
+            trabant.Megy(km1);
+
+            trabant.Allapot();
+
+            Console.ReadKey();
         }
     }
 }
